84e9b523345e648f234334e2f56ed06a83a07b47
Gal R <gal8156@gmail.com>
2024-11-28 18:55:13 +0200

    /

--- app/weather.py
+++ app/weather.py
@@ -14,7 +14,7 @@ def not_found(error):
 
 @app.errorhandler(401)
 def not_found(error): 
-    return render_template("401.html"), 401
+    return render_template("401.html"), 401 
 
 @app.errorhandler(500)
 def internal_error(error):
