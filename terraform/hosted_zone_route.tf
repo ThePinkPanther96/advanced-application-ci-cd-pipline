54d37e3e9c3f575fd6cbf9f145df6b4ac2a37254
Gal R <gal8156@gmail.com>
2024-11-30 20:13:26 +0200

    added hosted zone

--- terraform/hosted_zone_route.tf
+++ terraform/hosted_zone_route.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "myRecord" {
   zone_id = aws_route53_zone.myZone.zone_id
-  name    = "weather.gal-rozman.com"
+  name    = "weather"
   type    = "CNAME"
   ttl     = 300
   records = [aws_lb.app_lb.dns_name]
